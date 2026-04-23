a71b14aedf8f50852abd7fe6e0c0a6cb280169ef
RBBK7 <rbarahona2058@gmail.com>
2026-04-23 12:33:10 -0600

    Actualización inventarios

--- Muebleria_Proyecto/modules/inventario/eliminar.php
+++ Muebleria_Proyecto/modules/inventario/eliminar.php
@@ -1,23 +1,26 @@
 <?php
-
 require_once __DIR__ . '/../../Conexion/conexion.php';
 
-$db = new Database();
+$db   = new Database();
 $conn = $db->getConnection();
 
-$id = $_GET['id'];
+$id = $_GET['id'] ?? 0;
 
-$sql="DELETE FROM MUEBLERIA.INVENTARIO
-WHERE ID_INVENTARIO=:id";
-
-$stmt=oci_parse($conn,$sql);
-
-oci_bind_by_name($stmt,":id",$id);
-
+//PKG_INVENTARIO.SP_ELIMINAR_INVENTARIO
+$resultado = '';
+$stmt = oci_parse($conn,
+    "BEGIN PKG_INVENTARIO.SP_ELIMINAR_INVENTARIO(:id, :res); END;");
+oci_bind_by_name($stmt,':id', $id,32);
+oci_bind_by_name($stmt,':res',$resultado, 500);
 oci_execute($stmt);
-oci_commit($conn);
-
-header("Location: inventario.php");
 
 $db->close();
+
+if (strpos($resultado,'OK') === 0) {
+    header("Location: inventario.php");
+} else {
+    // Error redirige con mensaje en query string
+    header("Location: inventario.php?error=" . urlencode($resultado));
+}
+exit;
 ?>
